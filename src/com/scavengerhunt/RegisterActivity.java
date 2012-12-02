385bf22a68f4318ec00fc952a2976e5ce4d6ea6e
Sam Skinger <sjskinger@gmail.com>
2012-12-02 16:21:19 -0500

    register user updates

--- src/com/scavengerhunt/RegisterActivity.java
+++ src/com/scavengerhunt/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
 
@@ -15,7 +16,10 @@ public class RegisterActivity extends Activity {
 		Button toLoginScreen = (Button) findViewById(R.id.btnRegister);
         toLoginScreen.setOnClickListener(new View.OnClickListener() {
              public void onClick(View arg0) {
-                finish();
+                //add code here to check if info is valid before finishing
+            	 Toast.makeText(RegisterActivity.this, "You are now registered!",Toast.LENGTH_LONG).show();
+            	 
+            	 finish();
             }
         });
 	}
