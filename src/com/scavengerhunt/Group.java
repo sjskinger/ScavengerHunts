5bec476d8c605e07692cc7f6c668a1119d408c97
Sam Skinger <sjskinger@gmail.com>
2012-12-08 00:27:59 -0500

    Updates made to expandable lists. Expandable lists integrated into code for home page and "hunts I'm in" page.

---
+++ src/com/scavengerhunt/Group.java
@@ -0,0 +1,33 @@
+package com.scavengerhunt;
+
+import java.util.ArrayList;
+
+public class Group {
+	private String mTitle;
+	private ArrayList<Child> mArrayChildren;
+	
+	public Group() {}
+	
+	public Group(String mTitle,
+			ArrayList<Child> mArrayChildren) {
+		super();
+		this.mTitle = mTitle;
+		this.mArrayChildren = mArrayChildren;
+	}
+
+	public String getTitle() {
+        return mTitle;
+    }
+ 
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+ 
+    public ArrayList<Child> getArrayChildren() {
+        return mArrayChildren;
+    }
+ 
+    public void setArrayChildren(ArrayList<Child> mArrayChildren) {
+        this.mArrayChildren = mArrayChildren;
+    }
+}
